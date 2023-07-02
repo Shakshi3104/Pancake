9dd5ce850619d20857a640e0fad742388dca547c
Shakshi3104 <51497924+Shakshi3104@users.noreply.github.com>
2023-07-02 17:09:35 +0900

    Arrange padding and window size

--- Pancake/View/ContentView.swift
+++ Pancake/View/ContentView.swift
@@ -10,11 +10,13 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         SummaryView(pancakesData: PancakesData())
+            .padding(32)
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .frame(minWidth: 900, minHeight: 600)
     }
 }
