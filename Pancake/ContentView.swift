1bdddb81b1f87284d27d5c873cbee5e302389190
Shakshi3104 <51497924+Shakshi3104@users.noreply.github.com>
2023-07-02 16:18:17 +0900

    Initial Commit

---
+++ Pancake/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Pancake
+//
+//  Created by MacBook Pro M1 on 2023/07/02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Pancake/PancakeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PancakeApp.swift
+//  Pancake
+//
+//  Created by MacBook Pro M1 on 2023/07/02.
+//
+
+import SwiftUI
+
+@main
+struct PancakeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
