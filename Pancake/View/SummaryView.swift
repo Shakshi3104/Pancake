64b72a59e67ea1ed3869c833ae92d69797cd084c
Shakshi3104 <51497924+Shakshi3104@users.noreply.github.com>
2023-07-02 16:48:01 +0900

    Update sales count

---
+++ Pancake/View/SummaryView.swift
@@ -0,0 +1,18 @@
+//
+//  SummaryView.swift
+//  Pancake
+//
+//  Created by MacBook Pro M1 on 2023/07/02.
+//
+
+import SwiftUI
+
+struct SummaryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SummaryView()
+}
