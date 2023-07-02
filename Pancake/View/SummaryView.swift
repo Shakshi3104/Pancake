a2f4eba50d0ef6e4883b1dc0236a12958a86a166
Shakshi3104 <51497924+Shakshi3104@users.noreply.github.com>
2023-07-02 16:48:51 +0900

    Add SummaryView

--- Pancake/View/SummaryView.swift
+++ Pancake/View/SummaryView.swift
@@ -7,12 +7,51 @@
 
 import SwiftUI
 
+// MARK: - SummaryView
 struct SummaryView: View {
+    var pancakesData: PancakesData
+    @State private var showLineChartAverage = false
+    @State private var showBarChartAverage = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            // Title
+            HStack {
+                Text("Sales Summary")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                Spacer()
+            }
+            .padding()
+            
+            // Measure summary
+            HStack(spacing: 16) {
+                let totalSales = pancakesData.dailySalesPerCategory.reduce(0) { $0 + $1.sales }
+                let totalOrders = pancakesData.salesCategoryVsStore.reduce(0) { $0 + $1.sales }
+                let meanUnit = Int(totalSales / totalOrders)
+                
+                MeasureSummary(
+                    measureName: "Total Sales",
+                    measureValue: totalSales
+                )
+                
+                MeasureSummary(
+                    measureName: "Total Orders",
+                    measureValue: totalOrders
+                )
+                
+                MeasureSummary(
+                    measureName: "Average Unit",
+                    measureValue: meanUnit
+                )
+            }
+        }
     }
 }
 
-#Preview {
-    SummaryView()
+// MARK: - Preview
+struct SummaryView_Preview: PreviewProvider {
+    static var previews: some View {
+        SummaryView(pancakesData: PancakesData())
+    }
 }
